c92a0afd89b79f935217bf91f80f6c79dbe22ee7
ogre0382 <ogre0382@gmail.com>
2024-02-20 23:19:20 +0900

    upd

--- serve.py
+++ serve.py
@@ -2,8 +2,8 @@ import uvicorn
 import streamsync.serve
 from fastapi import FastAPI, Response
 
-root_asgi_app = FastAPI()
 sub_asgi_app = streamsync.serve.get_asgi_app("./testapp", "run")
+root_asgi_app = FastAPI(lifespan=sub_asgi_app.router.lifespan_context)
 
 root_asgi_app.mount("/testapp", sub_asgi_app)
 
@@ -13,8 +13,8 @@ async def init():
     <h1>Welcome to the App Hub</h1>
     """)
 
-uvicorn.run(root_asgi_app,
-    host="0.0.0.0",
-    port=5328,
-    log_level="warning",
-    ws_max_size=streamsync.serve.MAX_WEBSOCKET_MESSAGE_SIZE)
+# uvicorn.run(root_asgi_app,
+#     host="0.0.0.0",
+#     port=5328,
+#     log_level="warning",
+#     ws_max_size=streamsync.serve.MAX_WEBSOCKET_MESSAGE_SIZE)
